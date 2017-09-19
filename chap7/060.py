34cef2c7588f65c7b458d8b4fd90d0cb226f1485
shryics <tlab-pc-014@sd.tmu.ac.jp>
2017-09-19 18:04:35 +0900

    add 060,061,062(chap7)

---
+++ chap7/060.py
@@ -0,0 +1,20 @@
+import redis
+import json
+
+f = open('artist.json', 'r')
+
+r = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+
+for row in f:
+    data = json.loads(row)
+
+    if 'area' in data.keys():
+        r.set(data['name'], data['area'])
+        #print(data['name'], data['area'])
+    else:
+        r.set(data['name'], 'nothing')
+        #print(data['name'])
+f.close()
+
+print(r.keys())
